80bef9b993e1be09a8de299026489105b7da2196
tmampa <tmampa94@gmail.com>
2021-07-23 16:03:33 +0200

    Find friend by user id

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,6 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User'
+  has_many :confirmed_friendships, -> { where confirmed: true }, class_name: 'Friendship'
+  has_many :friends, through: :confirmed_friendships
 end
